0a0ab5047eb2c70352fea4c0ea4cd75bc0ac5b96
unknown <kimanijeff00@gmail.com>
2024-05-06 09:01:45 +0300

    final

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className='hidden xl:flex xl:max-w-none'>
+      <Image 
+        src={'/avatar.png'} 
+        width={500} 
+        height={500} 
+        alt='' 
+        className='tranlate-z-0 w-full h-full' />
+    </div>
+  );
 };
 
 export default Avatar;
